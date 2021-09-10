d985aa0bedb23af68365ca1e82a65abfea15d824
jincrates <94jingyu@gmail.com>
2021-09-10 16:12:50 +0900

    Initial commit

---
+++ src/test/java/com/jincrates/cogito/CogitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jincrates.cogito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CogitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
