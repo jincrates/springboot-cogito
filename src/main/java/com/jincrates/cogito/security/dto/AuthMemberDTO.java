5664c0fca8ddbf6afa6cf1868d81ac96ae4d67c9
jincrates <94jingyu@gmail.com>
2021-10-28 19:36:25 +0900

    Auth 회원가

--- src/main/java/com/jincrates/cogito/security/dto/AuthMemberDTO.java
+++ src/main/java/com/jincrates/cogito/security/dto/AuthMemberDTO.java
@@ -5,19 +5,35 @@ import lombok.ToString;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 
 @Log4j2
 @Getter
 @ToString
-public class AuthMemberDTO extends User {
+public class AuthMemberDTO extends User implements OAuth2User {
     //password는 부로 클래스를 사용하므로 별도의 멤버 변수로 선언하지 않음
     private String email;
+
+    private String password;
 
     private String name;
 
     private boolean fromSocial;
+
+    private Map<String, Object> attr;
+
+    public AuthMemberDTO(
+            String username,
+            String password,
+            boolean fromSocial,
+            Collection<? extends GrantedAuthority> authorities,
+            Map<String, Object> attr) {
+        this(username, password, fromSocial, authorities);
+        this.attr = attr;
+    }
 
     public AuthMemberDTO(
             String username,
@@ -35,5 +51,11 @@ public class AuthMemberDTO extends User {
 
     public void changeFromSocial(boolean fromSocial) {
         this.fromSocial = fromSocial;
+    }
+
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return this.attr;
     }
 }
