266736f1fea52e1abec08e8a9a7fbf6e79ad999e
jincrates <94jingyu@gmail.com>
2021-10-28 19:50:00 +0900

    RedirectStrategy를 이용한 리다이렉트

--- src/main/java/com/jincrates/cogito/config/SecurityConfig.java
+++ src/main/java/com/jincrates/cogito/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.jincrates.cogito.config;
 
+import com.jincrates.cogito.security.handler.LoginSuccessHandler;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
@@ -36,19 +37,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.formLogin();  //권한 인증에 문제시 로그인 화면 이동
         http.csrf().disable();  //csrf 토큰 비활성화
-        //http.logout();
-        http.oauth2Login(); //.successHandler(successHandler());
+
+        http.oauth2Login().successHandler(successHandler());
         //http.rememberMe().tokenValiditySeconds(60*60*24*7).userDetailsService(userDetailsService); //7dayss
     }
 
-    /*
-    더이상 사용하지 않음
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //사용자 계정은 user1
-        auth.inMemoryAuthentication().withUser("user1")
-                .password("$2a$10$IJqlyllQgWyv3G3EmxS/o.eBXYrT4W3NK2usZGygbhY4bKH67FCaK")
-                .roles("USER");
+    @Bean
+    public LoginSuccessHandler successHandler() {
+        return new LoginSuccessHandler(passwordEncoder());
     }
-    */
 }
